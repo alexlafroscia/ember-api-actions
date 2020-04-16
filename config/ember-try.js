236ae74a5c14fa56a5cc4b2a533daf60d0579b02
Alex LaFroscia <alex@lafroscia.com>
2020-04-16 10:32:24 -0400

    chore: run tests against Ember 3.12+
    BREAKING CHANGE: We are no longer actively testing against Ember versions lower than 3.12

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,26 +7,18 @@ module.exports = async function() {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-2.16',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-        },
+        name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-source': '~2.16.0'
+            'ember-source': '~3.12.0'
           }
         }
       },
       {
-        name: 'ember-lts-2.18',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-        },
+        name: 'ember-lts-3.16',
         npm: {
           devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-source': '~2.18.0'
+            'ember-source': '~3.16.0'
           }
         }
       },
